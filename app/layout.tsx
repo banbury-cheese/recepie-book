8903de0c1e66fd5f0f763b6fff736f90a819c5bf
Kay <kumartanmay315@gmail.com>
2024-06-17 18:46:13 +0530

    Adding fonts + init layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,30 +17,112 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <div className="menu">
-        <div className="menu__header">
-          <div className="menu__toggle">Close</div>
-          <Link href="/">Recipes for Food</Link>
-        </div>
-        <ul className="menu__contents">
-          <li className="menu__item">
-            <Link className="menu__item__link" href="/bisuits">
-              <p>
-                <b>A batch of bisuits</b>
-              </p>
-              <p>Anthony Zukofsky</p>
-            </Link>
-          </li>
-        </ul>
-        <div className="menu__footer">
-          <ul className="menu__footer__contents">
-            <li className="menu__footer__item">
-              <Link href="https://recipesforfood.net/about">About</Link>
+      <body className="layout-wrapper">
+        <div className="menu layout-wrapper__child">
+          <div className="menu__header">
+            <div className="menu__toggle">Close</div>
+            <Link href="/">Recipes for Food</Link>
+          </div>
+          <ul className="menu__contents">
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
+            </li>{" "}
+            <li className="menu__item">
+              <Link className="menu__item__link" href="/bisuits">
+                <p>
+                  <b>A batch of bisuits</b>
+                </p>
+                <p>Anthony Zukofsky</p>
+              </Link>
             </li>
           </ul>
+          <div className="menu__footer">
+            <ul className="menu__footer__contents">
+              <li className="menu__footer__item">
+                <Link href="https://recipesforfood.net/about">About</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-      <body className={inter.className}>{children}</body>
+        <div className="content layout-wrapper__child">{children}</div>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <>Recepie</>;
+  return <div className="recepie">Here's the recepie body</div>;
 }
