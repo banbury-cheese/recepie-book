491d18031ab0765c3c5b99ed12864f46de026c02
Kay <kumartanmay315@gmail.com>
2024-06-17 14:08:38 +0530

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,6 +17,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <div className="menu">
+        <div className="menu__header">
+          <div className="menu__toggle">Close</div>
+          <Link href="/">Recipes for Food</Link>
+        </div>
+        <ul className="menu__contents">
+          <li className="menu__item">
+            <Link className="menu__item__link" href="/bisuits">
+              <p>
+                <b>A batch of bisuits</b>
+              </p>
+              <p>Anthony Zukofsky</p>
+            </Link>
+          </li>
+        </ul>
+        <div className="menu__footer">
+          <ul className="menu__footer__contents">
+            <li className="menu__footer__item">
+              <Link href="https://recipesforfood.net/about">About</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
       <body className={inter.className}>{children}</body>
     </html>
   );
